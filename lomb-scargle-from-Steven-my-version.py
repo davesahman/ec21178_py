3aa94336d65de1c312ca8ee2984505afc0d7a0f6
davesahman <d.sahman@sheffield.ac.uk>
2019-02-01 15:48:39 +0000

    added gaussian fitting

--- lomb-scargle-from-Steven-my-version.py
+++ lomb-scargle-from-Steven-my-version.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from numpy import exp, linspace,random
 from astropy.stats import LombScargle
 import matplotlib.pyplot as plt
+import scipy
+from scipy.optimize import curve_fit
+import glob
+from hipercam.hlog import Hlog
+from astropy.convolution import convolve, Box1DKernel
+from astropy.stats import gaussian_fwhm_to_sigma
+
+# Set up gaussian model
+def gaussian(x, amp, cen, wid):
+    return amp * exp (-(x-cen)**2/(2*wid**2))
+
+def est_fwhm(x, y):
+    """
+    Estimate FWHM of a Gaussian
+    """
+    half_max = 0.5*y.max()
+    within_halfmax = y > half_max
+    x_within_halfmax = x[within_halfmax]
+    return x_within_halfmax.max() - x_within_halfmax.min()
+
+
 
 MJD        = 58324
 lcfile     = '../data/EC21178-5417.txt'
-Title      = 'WDJ044832.11-105349.85'
+Title      = 'EC21178'
 Norm_start = 0.701
 device     = '1/xs'
 period     = True
@@ -38,20 +60,42 @@ phase=fmax*x
 # print (phase[:10])
 phase=np.mod(phase,1)
 # print (phase[:100])
-   
-print("False alarm prob = {:.20f}".format(ls.false_alarm_probability(power.max())))
+# print("False alarm prob = {:.20f}".format(ls.false_alarm_probability(power.max())))
 
-fig,axs = plt.subplots(1,2,figsize=(10,7))
-axs[0].plot(freq,power)
-ax =axs[0]
-ax.set_xlabel("Freq ")
-ax.set_ylabel("Power")
-ax.set_title("Lomb-Scargle periodogram")
+a = x[55:90]
+b = y[55:90]
+c = e[55:90]
+print ('phase =',phase[55:90])
 
-axs[1].plot(phase,y,linewidth=0.2)
-ax =axs[1]
-ax.set_xlabel("Phase")
-ax.set_ylabel("Counts")
-ax.set_title("Phase folded light curve")
+# subtract off integer part of MJD
+# otherwise floating point errs in minimisation
+tfloor = int(a.min())
+a -= tfloor
 
+# Fit Gaussian to the min value of dfdt_ts
+#-------------------------------------------------------
+# find min values of b
+min_arg = np.argmin(b)
+t_min = a[min_arg]
+
+# get gaussian around peak to fit
+b -= b.max()
+g1 = np.fabs(b)
+
+# Gaussian fit
+# Initial guesses
+amp = -1 * b[min_arg]
+cen = t_min
+wid = est_fwhm(a, g1) * gaussian_fwhm_to_sigma
+init_vals = [amp ,cen, wid]
+best_vals, covar = curve_fit(gaussian, a, g1, p0=init_vals)
+
+# Plot results
+plt.plot(a, gaussian(a, *best_vals), label="fit")
+plt.plot(a, g1, label="data")
+plt.legend()
 plt.show()
+
+mid_ecl = tfloor + best_vals[1]
+mid_ecl_err = np.sqrt(covar[1,1])
+print("Eclipse Time = ",mid_ecl, "+/-", mid_ecl_err)
