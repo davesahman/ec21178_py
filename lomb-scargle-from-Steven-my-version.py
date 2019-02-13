c70a37e4e82c45799081ddbdac26e0d67bc8f1b3
david sahman <dis@davids-mbp.home>
2019-02-13 09:42:11 +0000

    sorting out missing eclispses

--- lomb-scargle-from-Steven-my-version.py
+++ lomb-scargle-from-Steven-my-version.py
@@ -45,14 +45,11 @@ period     = True
 
 x,y,e = np.loadtxt(lcfile, unpack=True, usecols=(0,1,2))
 
-# x -= MJD
+'''
+plt.plot(x,y)
+plt.show()
+'''
 
-# if len(x[x<Norm_start])>0:
-#    e /= y[x<Norm_start].mean()
-#    y /= y[x<Norm_start].mean()
-
-# plt.plot(x,y)
-# plt.show()
 
 if period:
     # x *= 1440.                                 # Change to minutes
@@ -63,13 +60,17 @@ if period:
     #freq = np.linspace(0.1,5,100)
     power = ls.power(freq)                      # Calculate periodogram powers            
     fmax  = freq[power==power.max()]        # Calculate peak in periodogram
-  #  print("Peak in periodogram at cycles / min. Period of minutes",1440*1/fmax) 
-  #  print("fmax =",fmax)
+# print("Peak in periodogram at cycles / day. Period of days",1/fmax) 
+# print("fmax =",fmax)
 
+period_time = 1/fmax
 phase=fmax*x
-# print (phase[:10])
 phase=np.mod(phase,1)
-# print (phase[:100])
+t2 = x - (period_time * int(x[0]/period_time)) # subtract integer of first period 
+cycle1 = np.floor_divide(t2,period_time)
+# print('cycl_1.shape =',cycle1.shape)
+
+# print ('phase  ',phase[:100])
 # print("False alarm prob = {:.20f}".format(ls.false_alarm_probability(power.max())))
 
 '''
@@ -113,7 +114,7 @@ print("Eclipse Time = ",mid_ecl, "+/-", mid_ecl_err)
 '''
 
 # create array with number of cycle
-
+'''
 cycle = np.zeros(phase.shape,dtype=int)
 cycle[0]=0
 # print ('range(len(phase))',range(len(phase)))
@@ -122,28 +123,43 @@ for i in range(len(phase)-1):
     if phase[i] > phase[i+1]:
         cycle[i+1] = cycle[i] + 1
 
-print('cycle.shape =',cycle.shape)
+# print('cycle.shape =',cycle.shape)
+'''
+# create list of bad or missed eclipses
+
+bad_ecl = [1]
 
 # loop over each cycle and fit gaussian
-ecl = np.zeros([cycle.shape[0],3],dtype=float)
+ecl = np.empty([cycle1.shape[0],3],dtype=float)
 
-i = 1
-# while i < cycle.max():
-while i < 20:
-    c_range = cycle == i
-    x_range = x[c_range]
-    y_range = y[c_range]
-    tfloor = int(x_range.min())
-    x_range -= tfloor
-    min_arg = np.argmin(y_range)
-    t_min = x_range[min_arg]
-    y_range -= y_range.max()
-    yr = np.fabs(y_range)
+i = 84
+# print('cycle1.max =',cycle1.max())
+# input("Press enter to continue......")
+# while i < (cycle1.max() - 1):
+while i < 100:
+    i += 1
+    if i in cycle1:
+      n=i
+      print('i before =',i)
+      if i == 86 or i ==93:  # this eclipse was not caught in the data
+         continue
+      print('i after =',i)
+      c_range = cycle1 == i
+      # print('c_range = ',c_range)
+      x_range = x[c_range]
+      # print('x_range = ',x_range)
+      y_range = y[c_range]
+      tfloor = int(x_range.min())
+      x_range -= tfloor
+      min_arg = np.argmin(y_range)
+      t_min = x_range[min_arg]
+      y_range -= y_range.max()
+      yr = np.fabs(y_range)
 
-    fwtm = 0.4*yr.max()
-    within_max = yr > fwtm
-    x_fwtm = x_range[within_max]
-    y_fwtm = yr[within_max]
+      fwtm = 0.4*yr.max()
+      within_max = yr > fwtm
+      x_fwtm = x_range[within_max]
+      y_fwtm = yr[within_max]
 
     # Gaussian fit
     # Initial guesses
@@ -154,25 +170,22 @@ while i < 20:
     best_vals, covar = curve_fit(gaussian, x_fwtm, y_fwtm, p0=init_vals)
 
     # Plot results
-    ''''
+   
     plt.plot(x_fwtm, gaussian(x_fwtm, *best_vals), label="fit")
     plt.plot(x_range, yr, label="data")
     plt.legend()
     plt.show()
-    '''
+  
     ecl[i,0] +=i
     ecl[i,1] +=best_vals[1] + tfloor
     ecl[i,2] +=np.sqrt(covar[1,1])
-    n=i
-    i += 1
-print('ecl =',ecl[0:n+1])
-print('ecl[n,1] =',ecl[n,1])
-print('ecl[1,1] =',ecl[1,1])
+# print('ecl =',ecl[0:n+1])
+#  print('ecl[n,1] =',ecl[n,1])
+#  print('ecl[1,1] =',ecl[1,1])
 
 ave_period = (ecl[n,1] - ecl[1,1])/(n-1)
 
-print('ave period = ',ave_period)
-'''
-plt.plot(ecl[1:n,0],ecl[1:n,1])
+# print('ave period = ',ave_period)
+
+plt.plot(ecl[1:n,0],ecl[1:n,1],'ro')
 plt.show()
-'''
